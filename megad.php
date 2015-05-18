2c066ca303c078e8853ee2fd55323cb360cbad3d
Sergei Jeihala <jey@tut.by>
2015-05-18 13:02:28 +0300

    megad.php fix

--- megad.php
+++ megad.php
@@ -2,24 +2,15 @@
 
 include_once("./config.php");
 include_once("./lib/loader.php");
-
-// start calculation of execution time
-startMeasure('TOTAL'); 
-
 include_once(DIR_MODULES."application.class.php");
-
-$session = new session("prj");
 
 // connecting to database
 $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); 
 
 include_once("./load_settings.php");
-
 include_once(DIR_MODULES.'megad/megad.class.php');
-
 $megad=new megad();
 $megad->processRequest();
-
 
 $db->Disconnect(); 
 
