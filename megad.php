91201b1793723ef40d9d9bb131cea7ab4b8cb5f9
SergeJey <sergejey@gmail.com>
2019-09-12 15:46:29 +0300

    Update megad.php
    Minor fix

--- megad.php
+++ megad.php
@@ -2,28 +2,32 @@
 @ini_set('zlib.output_compression', 'Off');
 @ini_set('output_buffering', 'Off');
 @ini_set('output_handler', '');
-@apache_setenv('no-gzip', 1);   
+
+if (function_exists('apache_setenv')) {
+    @apache_setenv('no-gzip', 1);
+}
+
 
 include_once("./config.php");
 include_once("./lib/loader.php");
-include_once(DIR_MODULES."application.class.php");
+include_once(DIR_MODULES . "application.class.php");
 
 // connecting to database
-$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); 
+$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 
 include_once("./load_settings.php");
-include_once(DIR_MODULES.'megad/megad.class.php');
-$megad=new megad();
+include_once(DIR_MODULES . 'megad/megad.class.php');
+$megad = new megad();
 $megad->getConfig();
 if ($megad->config['API_DEBUG']) {
- DebMes("Request: ".$_SERVER['REQUEST_URI']. " (".$_SERVER['REMOTE_ADDR'].")",'megad');
+    DebMes("Request: " . $_SERVER['REQUEST_URI'] . " (" . $_SERVER['REMOTE_ADDR'] . ")", 'megad');
 }
 $result = $megad->processRequest();
-if ($result!='') {
- echo $result;
+if ($result != '') {
+    echo $result;
 }
 if ($megad->config['API_DEBUG']) {
- DebMes("Result: ".$result,'megad');
+    DebMes("Result: " . $result, 'megad');
 }
 
 $db->Disconnect(); 
