7777b38228f985db44988a404ade39b889c3ef36
SergeJey <sergejey@gmail.com>
2019-09-23 16:59:09 +0300

    Update readvalues.inc.php
    Minor fix

--- modules/megad/readvalues.inc.php
+++ modules/megad/readvalues.inc.php
@@ -7,58 +7,58 @@ if ($all) {
     $stateData = $all;
 } else {
     if ($this->config['API_DEBUG']) {
-        DebMes("Reading state:\n".$url,'megad');
+        DebMes("Reading state:\n" . $url, 'megad');
     }
     $stateData = getURL($url, 0);
     if ($this->config['API_DEBUG']) {
-        DebMes("State response:\n".$stateData,'megad');
+        DebMes("State response:\n" . $stateData, 'megad');
     }
 }
 if ($_GET['debug']) {
-    dprint($url."\n".$stateData,false);
- }
-if ($stateData=='') return;
+    dprint($url . "\n" . $stateData, false);
+}
+if ($stateData == '') return;
 $commands = array();
 $states = explode(';', $stateData);
 $total = count($states);
 for ($i = 0; $i < $total; $i++) {
-    $matched=0;
-    $current_prop=SQLSelectOne("SELECT * FROM megadproperties WHERE DEVICE_ID='".$record['ID']."' AND NUM='".$i."' AND COMMAND_INDEX=0");
-    if (preg_match('/^i2c/',$current_prop['COMMAND'])) continue;
+    $matched = 0;
+    $current_prop = SQLSelectOne("SELECT * FROM megadproperties WHERE DEVICE_ID='" . $record['ID'] . "' AND NUM='" . $i . "' AND COMMAND_INDEX=0");
+    if (preg_match('/^i2c/', $current_prop['COMMAND'])) continue;
 
     if ($states[$i] == 'ON') {
         $cmd = array('NUM' => $i, 'VALUE' => 1, 'COMMAND' => 'output');
         $commands[] = $cmd;
-        $matched=1;
+        $matched = 1;
     }
     if ($states[$i] == 'OFF') {
         $cmd = array('NUM' => $i, 'VALUE' => 0, 'COMMAND' => 'output');
         $commands[] = $cmd;
-        $matched=1;
+        $matched = 1;
     }
     if (preg_match('/(ON|OFF)\/(\d+)/', $states[$i], $m)) {
         if ($m[1] == 'ON') {
             $cmd = array('NUM' => $i, 'VALUE' => 1, 'COMMAND' => 'input');
-         } else {
+        } else {
             $cmd = array('NUM' => $i, 'VALUE' => 0, 'COMMAND' => 'input');
-         }
+        }
         $commands[] = $cmd;
-        $matched=1;
+        $matched = 1;
         $cmd = array('NUM' => $i, 'VALUE' => $m[2], 'COMMAND' => 'counter');
         $commands[] = $cmd;
-        $matched=1;
+        $matched = 1;
     }
-    if (preg_match_all('/(temp|hum):([\-\d\.]+)/',$states[$i],$m)) {
-        $totalm=count($m[1]);
-        for($im=0;$im<$totalm;$im++) {
-            if ($m[1][$im]=='temp') {
-                $cmd = array('NUM' => $i, 'VALUE' => $m[2][$im], 'COMMAND' => 'temperature','INDEX'=>$im);
+    if (preg_match_all('/(temp|hum):([\-\d\.]+)/', $states[$i], $m)) {
+        $totalm = count($m[1]);
+        for ($im = 0; $im < $totalm; $im++) {
+            if ($m[1][$im] == 'temp') {
+                $cmd = array('NUM' => $i, 'VALUE' => $m[2][$im], 'COMMAND' => 'temperature', 'INDEX' => $im);
                 $commands[] = $cmd;
-                $matched=1;
+                $matched = 1;
             } else {
-                $cmd = array('NUM' => $i, 'VALUE' => $m[2][$im], 'COMMAND' => 'humidity','INDEX'=>$im);
+                $cmd = array('NUM' => $i, 'VALUE' => $m[2][$im], 'COMMAND' => 'humidity', 'INDEX' => $im);
                 $commands[] = $cmd;
-                $matched=1;
+                $matched = 1;
             }
         }
     }
@@ -74,10 +74,8 @@ for ($i = 0; $i < $total; $i++) {
 //internal temp sensor data
 $prop = SQLSelectOne("SELECT * FROM megadproperties WHERE DEVICE_ID='" . $record['ID'] . "' AND COMMAND='inttemp'");
 if (!$quick && $prop['ID']) {
-$url='http://' . $record['IP'] . '/' . $record['PASSWORD'] . '/?tget=1';
+    $url = 'http://' . $record['IP'] . '/' . $record['PASSWORD'] . '/?tget=1';
     $stateData = getURL($url, 0);
-    dprint($url."\n".$stateData,false);
-
     if ($stateData != '') {
         $commands[] = array('NUM' => 0, 'COMMAND' => 'inttemp', 'VALUE' => $stateData);
     }
@@ -87,43 +85,44 @@ $url='http://' . $record['IP'] . '/' . $record['PASSWORD'] . '/?tget=1';
 //internal GSM alarm 
 $prop = SQLSelectOne("SELECT * FROM megadproperties WHERE DEVICE_ID='" . $record['ID'] . "' AND COMMAND='alarm'");
 if (!$quick && $prop['ID']) {
-$url='http://' . $record['IP'] . '/' . $record['PASSWORD'] . '/?cf=1';
-$config=getURL($url, 0);
+    $url = 'http://' . $record['IP'] . '/' . $record['PASSWORD'] . '/?cf=1';
+    $config = getURL($url, 0);
 
-$preg=preg_match_all('/Mode: (.+?)<br>/m', $config,$m, PREG_SET_ORDER, 0);
-$mode=$m[0][1];
+    $preg = preg_match_all('/Mode: (.+?)<br>/m', $config, $m, PREG_SET_ORDER, 0);
+    $mode = $m[0][1];
 
-if ($mode=='disarm')        {$stateData='0';
-}
-if ($mode=='<b>ARM</b>')    {$stateData='1';}
+    if ($mode == 'disarm') {
+        $stateData = '0';
+    }
+    if ($mode == '<b>ARM</b>') {
+        $stateData = '1';
+    }
 
 
-    if (($stateData != '')&&(strlen($stateData))<20) {
+    if (($stateData != '') && (strlen($stateData)) < 20) {
         //echo "arm: ".$stateData."<br>";
         $commands[] = array('NUM' => 100, 'COMMAND' => 'alarm', 'VALUE' => $stateData);
     }
 }
 
 
-
-
-$i2c_properties=SQLSelect("SELECT * FROM megadproperties WHERE DEVICE_ID='".$record['ID']."' AND COMMAND LIKE 'i2c%' ORDER BY NUM");
-if (!$quick && $stateData!='' && $i2c_properties[0]['ID']) {
-    include_once(DIR_MODULES.$this->name.'/libs/i2c_com.class.php');
-    include_once(DIR_MODULES.$this->name.'/libs/i2c_functions.inc.php');
-    foreach($i2c_properties as $property) {
-        $scl=$property['NUM'];
-        $sda=$property['ADD_NUM'];
-        $i2c_com=new i2c_com('http://'.$record['IP'].'/'.$record['PASSWORD'].'/?',$scl,$sda,$record['I2C_VERSION']);
-        if ($property['COMMAND']=='i2c_htu21d') {
-            include_once(DIR_MODULES.$this->name.'/libs/i2c_htu21d.inc.php');
+$i2c_properties = SQLSelect("SELECT * FROM megadproperties WHERE DEVICE_ID='" . $record['ID'] . "' AND COMMAND LIKE 'i2c%' ORDER BY NUM");
+if (!$quick && $stateData != '' && $i2c_properties[0]['ID']) {
+    include_once(DIR_MODULES . $this->name . '/libs/i2c_com.class.php');
+    include_once(DIR_MODULES . $this->name . '/libs/i2c_functions.inc.php');
+    foreach ($i2c_properties as $property) {
+        $scl = $property['NUM'];
+        $sda = $property['ADD_NUM'];
+        $i2c_com = new i2c_com('http://' . $record['IP'] . '/' . $record['PASSWORD'] . '/?', $scl, $sda, $record['I2C_VERSION']);
+        if ($property['COMMAND'] == 'i2c_htu21d') {
+            include_once(DIR_MODULES . $this->name . '/libs/i2c_htu21d.inc.php');
             $temperature = get_htu21d_temperature($i2c_com);
             if (is_numeric($temperature)) {
-                $commands[] = array('NUM' => $property['NUM'], 'COMMAND' => 'temperature','INDEX'=>1, 'VALUE' => $temperature);
+                $commands[] = array('NUM' => $property['NUM'], 'COMMAND' => 'temperature', 'INDEX' => 1, 'VALUE' => $temperature);
                 $humidity = get_htu21d_humidity($i2c_com);
                 if (is_numeric($humidity)) {
-                    $hum_compensated = round($humidity + (25 - $temperature) * -0.15,2);
-                    $commands[] = array('NUM' => $property['NUM'], 'COMMAND' => 'humidity','INDEX'=>1, 'VALUE' => $hum_compensated);
+                    $hum_compensated = round($humidity + (25 - $temperature) * -0.15, 2);
+                    $commands[] = array('NUM' => $property['NUM'], 'COMMAND' => 'humidity', 'INDEX' => 1, 'VALUE' => $hum_compensated);
                 }
             }
         }
@@ -131,8 +130,10 @@ if (!$quick && $stateData!='' && $i2c_properties[0]['ID']) {
 }
 
 foreach ($commands as $command) {
-    $this->processCommand($record['ID'],$command);
+    $this->processCommand($record['ID'], $command);
 
 }
 
-if ($_GET['debug']) {    dprint($commands);}
+if ($_GET['debug']) {
+    dprint($commands);
+}
